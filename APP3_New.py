66c0a2cad46abf9851f61b9685ef4d1034343cc2
arifiqbal74 <123804745+arifiqbal74@users.noreply.github.com>
2023-02-18 17:05:05 +0500

    Add files via upload

---
+++ APP3_New.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 21:53:12 2023
+
+@author: Asif Iqbal
+"""
+
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+
+# load data sets
+athletes = pd.read_csv(r'C:\Users\Lenovo\Desktop\Class 1 Python\Assignments\3rd Assignment\archive (1)\athlete_events.csv') 
+regions = pd.read_csv(r'C:\Users\Lenovo\Desktop\Class 1 Python\Assignments\3rd Assignment\archive (1)\noc_regions.csv')
+
+print(athletes.head())
+
+print(regions.head())
+
+# Join the Dataframes on base of NOC Column
+athletes_df = athletes.merge(regions, how='left', on = 'NOC')
+print(athletes_df.head())
+
+# how many rows and columns
+athletes_df.shape
+
+# rename last two columns with upper case(first letter)
+athletes_df.rename(columns={'region':'Region', 'notes':'Notes'},inplace=True)
+print(athletes_df.head())
+
+# data type
+athletes_df.info()
+
+# numerical values of the dataframes
+athletes_df.describe()
+
+# null values count
+athletes_df.isna().sum()
+
+#Replace the null values of “Age” column by 0.
+athletes_df["Age"].replace(np.nan, 0, inplace=True)
+athletes_df['Age'].unique()
+
+#Replace the null value of “Height” column by the last value.
+athletes_df['Height'].fillna(method='ffill',inplace=True)
+athletes_df['Height'].unique()
+
+## for missing "Weight" value, replace it with rounded mean value.
+athletes_df['Weight'].fillna(round(athletes_df.Weight.mean()), inplace=True)
+
+athletes_df.info()
+
+#Replace the null values of Medal column by 0.
+athletes_df["Medal"].replace(np.nan, 0, inplace=True)
+athletes_df['Medal'].unique()
+
+
+athletes_df.info()
+
+import streamlit as st
+
+#st.set_page_config(layout="wide")
+
+st.title('Olympics Dashboard')
+
+# use year to filter data
+year = st.selectbox('Select year', athletes_df['Year'])
+
+# the metric component takes the value you want to show and the change from a prev. value (it shows it as up/down arrow based on the change value)
+curr_count = 100
+inc_count = 10
+
+curr_medals = 50
+inc_medals = -4
+
+country_count = 14
+inc_count = 5
+countries = athletes_df['Region'].nunique()
+gold_medals = 13732
+Silver_medal = 13116
+bronze_medal = 13295 
+
+# combining metrics and columns to create 
+st.header('Olympics - {}'.format(year))
+col1, col2, col3, col4, col5 = st.columns(5)
+col1.metric('Number of Olympians', athletes_df['ID'].nunique())
+col2.metric('Participating Countries', countries)
+col3.metric('Gold Medals', gold_medals)
+col4.metric('Silver Medals', Silver_medal)
+col5.metric('Bronze Medals', bronze_medal)
+
+
+subset = athletes_df[athletes_df['Year'] == year]
+st.dataframe(subset)
+

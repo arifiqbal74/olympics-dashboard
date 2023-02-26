41cbb03495e3d4fadac7e1aafa48554d06988f12
arifiqbal74 <123804745+arifiqbal74@users.noreply.github.com>
2023-02-26 22:58:57 +0500

    Update APP3_New.py

--- APP3_New.py
+++ APP3_New.py
@@ -94,8 +94,8 @@ col4.metric('Silver Medals', Silver_medal)
 col5.metric('Bronze Medals', bronze_medal)
 
 
-#subset = athletes_df[athletes_df['Year'] == year]
-#st.dataframe(subset)
+subset = athletes_df[athletes_df['Year'] == year]
+st.dataframe(subset)
 
 # Creating Visuals 
 

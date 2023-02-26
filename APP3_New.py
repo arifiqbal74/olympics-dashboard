0323da8aeed3af3b6977f5df15c9a8a73f75bc46
arifiqbal74 <123804745+arifiqbal74@users.noreply.github.com>
2023-02-26 23:11:58 +0500

    Update APP3_New.py

--- APP3_New.py
+++ APP3_New.py
@@ -93,7 +93,6 @@ col3.metric('Gold Medals', gold_medals)
 col4.metric('Silver Medals', Silver_medal)
 col5.metric('Bronze Medals', bronze_medal)
 
-
 subset = athletes_df[athletes_df['Year'] == year]
 st.dataframe(subset)
 

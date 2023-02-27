9af21fb13ac4332afa41ae95c936678e0ace0aad
arifiqbal74 <123804745+arifiqbal74@users.noreply.github.com>
2023-02-27 22:05:52 +0500

    Update APP3_New.py

--- APP3_New.py
+++ APP3_New.py
@@ -6,9 +6,9 @@ Created on Sun Feb 12 21:53:12 2023
 """
 
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # load data sets
 athletes = pd.read_csv('https://raw.githubusercontent.com/arifiqbal74/olympics-dashboard/main/athlete_events_till_150K.csv') 
